e2e93e093d963db3f659f936cc01bdefc5706752
reeturn01HPC <test@email.com>
2021-07-26 22:07:51 +0200

    Направена класа за Типови на инсулин

---
+++ ProjectVP-DiabetesLog/InsulinType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectVP_DiabetesLog
+{
+    public class InsulinType
+    {
+        public string[] Types{ get; set; }
+
+        public InsulinType()
+        {
+            Types = new string[]
+            {
+                "Apidra",
+                "Lantus",
+                "Levemir"
+            };
+        }
+    }
+}
