1b6f336f92b53fb1afac7880f7cc2249501a714a
reeturn01HPC <test@email.com>
2021-08-03 18:13:38 +0200

    Додаден connectionString за LoggerDB.db во App.config

---
+++ ProjectVP-DiabetesLog/DatabaseAccess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace ProjectVP_DiabetesLog
+{
+    class DatabaseAccess
+    {
+        public string LoadConnectionString(string name = "Default") {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
