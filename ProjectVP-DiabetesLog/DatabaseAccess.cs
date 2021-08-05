b343166d57fed6f8bae9a432dab5b9f87dbd742c
reeturn01HPC <test@email.com>
2021-08-05 23:22:26 +0200

    Додадена класа Meal, додадени функции за работење со датабазата LoggerDB и направена функционалноста на копчето Додади запис( се додава нов запис врз основ на селектираните опции)

--- ProjectVP-DiabetesLog/DatabaseAccess.cs
+++ ProjectVP-DiabetesLog/DatabaseAccess.cs
@@ -4,13 +4,221 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
+using System.Data.SQLite;
 
 namespace ProjectVP_DiabetesLog
 {
-    class DatabaseAccess
+    public static class DatabaseAccess
     {
-        public string LoadConnectionString(string name = "Default") {
+        public static int InsertDateTime(DateTime date, DateTime time) {
+            int rowId = 0;
+
+            using (SQLiteConnection connection = new SQLiteConnection(LoadConnectionString()))
+            {
+                string querry = "INSERT INTO Measurement (Date, Time) VALUES (@Date, @Time)";
+                using (SQLiteCommand command = new SQLiteCommand(querry, connection))
+                {
+                    querry = "SELECT last_insert_rowid()";
+                    using (SQLiteCommand readCommand = new SQLiteCommand(querry, connection))
+                    {
+                        command.Parameters.Add("@Date", System.Data.DbType.String).Value = date.ToString("yyyy-MM-dd");
+                        command.Parameters.Add("@Time", System.Data.DbType.String).Value = time.ToString("HH:mm");
+                        
+                        connection.Open();
+                        command.ExecuteNonQuery();
+
+                        using (SQLiteDataReader reader = readCommand.ExecuteReader())
+                        {
+                            reader.Read();
+                            rowId = reader.GetInt32(0);
+                            reader.Close();
+                        }
+
+                        connection.Close();
+                    }
+                }
+
+            }
+            return rowId;
+        }
+
+        public static List<InsulinType> LoadInsulinTypes() {
+            List<InsulinType> insulinTypes = new List<InsulinType>();
+            using (SQLiteConnection connection = new SQLiteConnection(LoadConnectionString()))
+            {
+                using (SQLiteCommand command = new SQLiteCommand("SELECT Name, Brand FROM Insulin", connection))
+                {
+                    connection.Open();
+                    using (SQLiteDataReader reader= command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            insulinTypes.Add(new InsulinType(reader.GetString(0), reader.GetString(1)));
+                        }
+                        reader.Close();
+                    }
+                    connection.Close();
+                }
+            }
+
+            return insulinTypes;
+        }
+
+        public static List<Food> LoadFoodTypes()
+        {
+            List<Food> foods = new List<Food>();
+            using (SQLiteConnection connection = new SQLiteConnection(LoadConnectionString()))
+            {
+                using (SQLiteCommand command = new SQLiteCommand("SELECT Name, Brand, Carbs FROM Food", connection))
+                {
+                    connection.Open();
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string name = reader.GetString(0);
+                            string brand = reader.GetString(1);
+                            double carbs = reader.GetDouble(2);
+                            foods.Add(new Food(name, brand, carbs));
+                        }
+                        reader.Close();
+                    }
+                    connection.Close();
+                }
+            }
+            return foods;
+        }
+
+        public static void InsertFoodType(Food foodToAdd)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(LoadConnectionString()))
+            {
+                using (SQLiteCommand command = new SQLiteCommand("INSERT INTO Food (Name, Brand, Carbs) VALUES (@Name, @Brand, @Carbs)", connection))
+                {
+                    command.Parameters.Add("@Name", System.Data.DbType.String).Value = foodToAdd.name;
+                    command.Parameters.Add("@Brand", System.Data.DbType.String).Value = foodToAdd.brand;
+                    command.Parameters.Add("@Carbs", System.Data.DbType.Double).Value = foodToAdd.carbs;
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+        }
+
+        public static void InsertInsulinType(InsulinType tmp)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(LoadConnectionString()))
+            {
+                using (SQLiteCommand command = new SQLiteCommand("INSERT INTO Insulin (Name, Brand) VALUES (@Name, @Brand)", connection))
+                {
+                    command.Parameters.Add("@Name", System.Data.DbType.String).Value = tmp.name;
+                    command.Parameters.Add("@Brand", System.Data.DbType.String).Value = tmp.brand;
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+        }
+
+        private static string LoadConnectionString(string name = "Default") {
             return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+
+        public static void InsertMeasurement(int rowId, double value)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(LoadConnectionString()))
+            {
+                using (SQLiteCommand command = new SQLiteCommand("UPDATE Measurement SET MeasuredValue = @Value WHERE MeasurementId = @RowId", connection))
+                {
+                    command.Parameters.Add("@Value", System.Data.DbType.Double).Value = value;
+                    command.Parameters.Add("@RowId", System.Data.DbType.Int32).Value = rowId;
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+        }
+
+        public static void insertMeal(int rowId, Meal meal)
+        {
+            int foodId;
+            using (SQLiteConnection connection = new SQLiteConnection(LoadConnectionString()))
+            {
+                using (SQLiteCommand command = new SQLiteCommand("SELECT FoodId FROM Food WHERE Name = @Name AND Brand = @Brand", connection))
+                {
+                    command.Parameters.Add("@Name", System.Data.DbType.String).Value = meal.food.name;
+                    command.Parameters.Add("@Brand", System.Data.DbType.String).Value = meal.food.brand;
+                    connection.Open();
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        reader.Read();
+                        foodId = reader.GetInt32(0);
+                        reader.Close();
+                    }
+                    connection.Close();
+                }
+                using (SQLiteCommand command = new SQLiteCommand("INSERT INTO MeasurementFood (MeasurementId, FoodId, AmountInGrams) VALUES (@MeasurementId, @FoodId, @AmountInGrams)",connection))
+                {
+                    command.Parameters.Add("@MeasurementId", System.Data.DbType.Int32).Value = rowId;
+                    command.Parameters.Add("@FoodId", System.Data.DbType.Int32).Value = foodId;
+                    command.Parameters.Add("@AmountInGrams", System.Data.DbType.Double).Value = meal.amount;
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+        }
+
+        public static void InsertInsulinMeasurement(int rowId, int value, InsulinType insulin)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(LoadConnectionString()))
+            {
+                int insulinId = -1;
+                int addedInsulinId = -1;
+                using (SQLiteCommand command = new SQLiteCommand("SELECT InsulinId FROM Insulin WHERE Name = @Name AND Brand = @Brand", connection))
+                {
+                    command.Parameters.Add("@Name", System.Data.DbType.String).Value = insulin.name;
+                    command.Parameters.Add("Brand", System.Data.DbType.String).Value = insulin.brand;
+                    connection.Open();
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        reader.Read();
+                        insulinId = reader.GetInt32(0);
+                        reader.Close();
+                    }
+                    connection.Close();
+                }
+                
+                using (SQLiteCommand insertCommand = new SQLiteCommand("INSERT INTO AddedInsulin (InsulinId, InsulinAmount) VALUES (@InsulinId, @InsulinAmount)", connection))
+                {
+                    insertCommand.Parameters.Add("@InsulinId", System.Data.DbType.Int32).Value = insulinId;
+                    insertCommand.Parameters.Add("@InsulinAmount", System.Data.DbType.Int32).Value = value;
+                    connection.Open();
+                    insertCommand.ExecuteNonQuery();
+                    using (SQLiteCommand returnIdCommand = new SQLiteCommand("SELECT last_insert_rowid()", connection))
+                    {
+                        using (SQLiteDataReader reader = returnIdCommand.ExecuteReader())
+                        {
+                            reader.Read();
+                            addedInsulinId = reader.GetInt32(0);
+                            reader.Close();
+                        }
+                    }
+                    connection.Close();
+                }
+   
+                
+                using (SQLiteCommand command = new SQLiteCommand("UPDATE Measurement SET AddedInsulinId = @AddedInsulinId WHERE MeasurementId = @RowId", connection))
+                {
+                    command.Parameters.Add("@AddedInsulinId", System.Data.DbType.Int32).Value = addedInsulinId;
+                    command.Parameters.Add("@RowId", System.Data.DbType.Int32).Value = rowId;
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
         }
     }
 }
--- ProjectVP-DiabetesLog/FormAddMeasurement.cs
+++ ProjectVP-DiabetesLog/FormAddMeasurement.cs
@@ -14,18 +14,27 @@ namespace ProjectVP_DiabetesLog
     public partial class FormAddMeasurement : Form
     {
         public static List<InsulinType> insulinTypes;
+        public static List<Food> foods;
+        private List<Meal> meals;
         public FormAddMeasurement()
         {
             InitializeComponent();
 
             //Moi initializacii
-            insulinTypes = new List<InsulinType>();
+            meals = new List<Meal>();
+            insulinTypes = DatabaseAccess.LoadInsulinTypes();
+            foods = DatabaseAccess.LoadFoodTypes();
 
             Initialize_dtp_Date();
             Initialize_dtp_Time();
 
             Initialize_cb_InsulinType();
+            Initialize_cb_Food();
+        }
 
+        private void Initialize_cb_Food()
+        {
+            cb_Food.DataSource = foods;
         }
 
         private void Initialize_cb_InsulinType()
@@ -109,18 +118,31 @@ namespace ProjectVP_DiabetesLog
             FormAddFoodType formAddFoodType = new FormAddFoodType();
             if (formAddFoodType.ShowDialog() == DialogResult.OK)
             {
-                cb_Food.Items.Add(formAddFoodType.foodToAdd);
+                foods.Add(formAddFoodType.foodToAdd);
+                cb_Food.DataSource = null;
+                cb_Food.DataSource = foods;
             }
         }
 
         private void btn_AddMeal_Click(object sender, EventArgs e)
         {
+            errorProvider_AddMeasurement.Clear();
             if (cb_Food.SelectedIndex != -1)
             {
-                Food tmp = (Food)cb_Food.SelectedItem;
-                ListViewItem viewItem = new ListViewItem(tmp.ToArray());
-                viewItem.SubItems.Add(nud_AmountMeal.Value.ToString());
-                listView_Meals.Items.Add(viewItem);
+                if (nud_AmountMeal.Value.Equals(0))
+                {
+                    errorProvider_AddMeasurement.SetError(nud_AmountMeal, "Внесете количина!");
+                }
+                else
+                {
+                    Food tmp = (Food)cb_Food.SelectedItem;
+                    meals.Add(new Meal(tmp, Decimal.ToInt32(nud_AmountMeal.Value)));
+
+                    ListViewItem viewItem = new ListViewItem(tmp.ToArray());
+                    viewItem.SubItems.Add(nud_AmountMeal.Value.ToString());
+                    listView_Meals.Items.Add(viewItem);
+                }
+                
             }
         }
 
@@ -130,6 +152,66 @@ namespace ProjectVP_DiabetesLog
             {
                 listView_Meals.Items.Remove(listView_Meals.SelectedItems[0]);
             }
+        }
+
+        private void dtp_Date_ValueChanged(object sender, EventArgs e)
+        {
+           // DatabaseAccess.DateRecords(dtp_Date.Value.Date);
+        }
+
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_Add_Click(object sender, EventArgs e)
+        {
+            
+            if (!CheckForErrors())
+            {
+                
+                int rowId = DatabaseAccess.InsertDateTime(dtp_Date.Value.Date, dtp_Time.Value);
+                
+                if (checkBox_EnableMeasurement.Checked.Equals(true))
+                {
+                    DatabaseAccess.InsertMeasurement( rowId, Decimal.ToDouble(nud_MeasuredValue.Value));
+                }
+                if (checkBox_EnableInsulin.Checked.Equals(true))
+                {
+                    DatabaseAccess.InsertInsulinMeasurement(rowId, Decimal.ToInt32(nud_InsulinAmount.Value), (InsulinType)cb_InsulinType.SelectedItem);
+                }
+                if (checkBox_EnableMeal.Checked.Equals(true))
+                {
+                    foreach (Meal meal in meals)
+                    {
+                        DatabaseAccess.insertMeal(rowId, meal);
+                    }
+                }
+                MessageBox.Show(rowId.ToString());
+            }
+            
+        }
+
+        private bool CheckForErrors()
+        {
+            errorProvider_AddMeasurement.Clear();
+            bool ErrorSet = false;
+            if (checkBox_EnableMeasurement.Checked == true && nud_MeasuredValue.Value.Equals(0))
+            {
+                errorProvider_AddMeasurement.SetError(nud_MeasuredValue, "Внесете вредност!");
+                ErrorSet = true;
+            }
+            if (checkBox_EnableInsulin.Checked == true && nud_InsulinAmount.Value.Equals(0))
+            {
+                errorProvider_AddMeasurement.SetError(nud_InsulinAmount, "Внесете количина!");
+                ErrorSet = true;
+            }
+            if (checkBox_EnableMeal.Checked == true && listView_Meals.Items.Count <= 0)
+            {
+                errorProvider_AddMeasurement.SetError(checkBox_EnableMeal, "Немате додадено оброци!");
+                ErrorSet = true;
+            }
+            return ErrorSet;
         }
     }
 }
