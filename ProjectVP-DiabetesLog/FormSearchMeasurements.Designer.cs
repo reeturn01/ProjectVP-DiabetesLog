b8bf9c1348e6fdcd0fd9a067b4d8ea24f104e095
reeturn01HPC <test@email.com>
2021-08-28 21:43:13 +0200

    Dodadena klasa InsulinAdded

--- ProjectVP-DiabetesLog/FormSearchMeasurements.Designer.cs
+++ ProjectVP-DiabetesLog/FormSearchMeasurements.Designer.cs
@@ -45,6 +45,7 @@ namespace ProjectVP_DiabetesLog
             this.dtp_Date.Name = "dtp_Date";
             this.dtp_Date.Size = new System.Drawing.Size(200, 20);
             this.dtp_Date.TabIndex = 0;
+            this.dtp_Date.ValueChanged += new System.EventHandler(this.dtp_Date_ValueChanged);
             // 
             // dgv_MeasurementsList
             // 
