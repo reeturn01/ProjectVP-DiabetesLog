1dcc13b001dd8416de14f07c998170325bcd235a
reeturn01HPC <test@email.com>
2021-08-28 22:18:23 +0200

    Napravena klasa MeasurementsOnDate i dodadeno pole DateTime time vo klasata TimeMeasurements

---
+++ ProjectVP-DiabetesLog/MeasurementsOnDate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectVP_DiabetesLog
+{
+    class MeasurementsOnDate
+    {
+        public DateTime date { get; private set; }
+        public List<TimeMeasurement> timeMeasurements { get; private set; }
+
+        public MeasurementsOnDate(DateTime date) {
+            this.date = date;
+            timeMeasurements = new List<TimeMeasurement>();
+        }
+        public MeasurementsOnDate(DateTime date, List<TimeMeasurement> timeMeasurements) {
+            this.date = date;
+            this.timeMeasurements = timeMeasurements;
+        }
+
+        public void addTimeMeasurement(TimeMeasurement measurementToAdd) {
+            timeMeasurements.Add(measurementToAdd);
+        }
+    }
+}
