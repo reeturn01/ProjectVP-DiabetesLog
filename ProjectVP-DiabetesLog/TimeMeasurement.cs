776fb8bf460858e392fea8ef3ba393407c623a80
reeturn01HPC <test@email.com>
2021-08-28 22:18:23 +0200

    Napravena klasa MeasurementsOnDate i dodadeno pole DateTime time vo klasata TimeMeasurements

--- ProjectVP-DiabetesLog/TimeMeasurement.cs
+++ ProjectVP-DiabetesLog/TimeMeasurement.cs
@@ -8,17 +8,20 @@ namespace ProjectVP_DiabetesLog
 {
     public class TimeMeasurement
     {
+        public DateTime time { get; private set; }
         public double measurement { get; private set; }
         public InsulinAdded insulinAdded { get; private set; }
         public List<Meal> meals { get; private set; }
 
-        public TimeMeasurement() {
+        public TimeMeasurement(DateTime time) {
+            this.time = time;
             measurement = 0;
             insulinAdded = new InsulinAdded();
             meals = new List<Meal>();
         }
 
-        public TimeMeasurement(double measurement, InsulinAdded insulinAdded, List<Meal> meals) {
+        public TimeMeasurement(DateTime time, double measurement, InsulinAdded insulinAdded, List<Meal> meals) {
+            this.time = time;
             this.measurement = measurement;
             this.insulinAdded = insulinAdded;
             this.meals = meals;
